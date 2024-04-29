e02948cd2bbed781a5328efc7ddfb97abc430067
DynamicDaikon <hietasoba@gmail.com>
2024-04-29 14:41:17 +0900

    text from 3-4 to 4-2

---
+++ directive-bind-object/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const imgAttributes = ref({
+  src: './images/logo.svg',
+  alt: 'Vueのロゴ',
+  width: 75,
+  height: 75
+})
+</script>
+
+<template>
+  <p>
+    <img
+      :src="imgAttributes.src"
+      :alt="imgAttributes.alt"
+      :width="imgAttributes.width"
+      :height="imgAttributes.height"
+    />
+  </p>
+  <p><img v-bind:imgAttributes /></p>
+  <p><img v-bind:imgAttributes title="ロゴです！" /></p>
+  <p><img v-bind:imgAttributes alt="ロゴです！" /></p>
+</template>
+
+<style scoped></style>
