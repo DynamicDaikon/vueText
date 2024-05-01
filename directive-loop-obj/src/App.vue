3c67f42d968bfda0c1d83ede6c8b002f6ea93712
DynamicDaikon <hietasoba@gmail.com>
2024-05-01 16:35:48 +0900

    from 6-1 to 6-2 comp

---
+++ directive-loop-obj/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const whiteLadyInit: {
+  id: number
+  name: string
+  price: number
+  recipe: string
+} = {
+  id: 2345,
+  name: 'ホワイトレディ',
+  price: 1200,
+  recipe: 'ジン30ml+コワントロー15ml+レモン果汁+15ml'
+}
+
+const whiteLady = ref(whiteLadyInit)
+</script>
+
+<template>
+  <dl>
+    <template v-for="(value, key) in whiteLady" v-bind:key="key">
+      <dt>{{ key }}</dt>
+      <dt>{{ value }}</dt>
+    </template>
+  </dl>
+</template>
+
+<style scoped></style>
