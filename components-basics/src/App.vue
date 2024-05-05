bfea72cf4a733e49bce8d0a0c216bec95a436c01
DynamicDaikon <hietasoba@gmail.com>
2024-05-05 21:31:14 +0900

    from 8-1 start to 8-1 end

---
+++ components-basics/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import OneSections from './components/OneSections.vue';
+</script>
+
+<template>
+  <h1>コンポーネント基礎</h1>
+  <section>
+    <h2>コンポーネント1個</h2>
+    <OneSections/>
+  </section>
+  <section>
+    <h2>コンポーネントが複数</h2>
+    <OneSections/>
+    <OneSections/>
+    <OneSections/>
+  </section>
+</template>
+
+<style scoped>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
