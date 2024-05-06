e6c0ab5833e28f172148262e2eba41f5c4296297
DynamicDaikon <hietasoba@gmail.com>
2024-05-06 16:31:14 +0900

    from 8-3 start to 8-3 end

---
+++ components-props-basics/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import OneInfo from './components/OneInfo.vue';
+</script>
+
+<template>
+  <section>
+    <h1>Propsの基礎</h1>
+    <OneInfo
+    title="Propsの利用"
+    content="子コンポーネントにデータを渡すにはPropsを利用する。"/>
+  </section>
+</template>
+
+<style scoped>
+section {
+  border:blue 1px solid;
+  margin:10px;
+}
+</style>
---
+++ components-props-basics/src/components/OneInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="box">
+    <h4>{{ title }}</h4>
+    <p>{{ content }}</p>
+  </section>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+  content: string;
+}
+
+defineProps<Props>();
+</script>
+
+<style scoped>
+.box {
+  border: green 1px dashed;
+  margin: 10px;
+}
+</style>
