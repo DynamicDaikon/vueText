d45bfc7602e54a74e34b105e7ad33ef54592070f
DynamicDaikon <hietasoba@gmail.com>
2024-05-12 16:55:58 +0900

    from 9-1-3 start to 9 end

---
+++ components-dynamic/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Input from './components/Input.vue'
+import Radio from './components/Radio.vue'
+import Select from './components/Select.vue'
+
+const currentComp = ref(Input)
+const currentCompName = ref('Input')
+const compNameList: string[] = ['Input', 'Radio', 'Select']
+const compList = [Input, Radio, Select]
+let currentCompIndex = 0
+const switchComp = (): void => {
+  currentCompIndex++
+
+  if (currentCompIndex >= 3) {
+    currentCompIndex = 0
+  }
+  currentComp.value = compList[currentCompIndex]
+  currentCompName.value = compNameList[currentCompIndex]
+}
+</script>
+
+<template>
+  <p>コンポーネント名：{{ currentCompName }}</p>
+  <KeepAlive>
+    <component v-bind:is="currentComp" />
+  </KeepAlive>
+  <button v-on:click="switchComp">切り替え</button>
+</template>
+
+<style scoped></style>
---
+++ components-dynamic/src/components/Input.vue
@@ -0,0 +1,9 @@
+<template>
+  <input type="text" v-model="inputNameModel" />
+  <p>{{ inputNameModel }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const inputNameModel = ref('田中太郎')
+</script>
