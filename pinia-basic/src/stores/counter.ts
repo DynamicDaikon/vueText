bb8ac59eacb517c0926908b6d493fb5050827117
DynamicDaikon <hietasoba@gmail.com>
2024-05-18 23:15:25 +0900

    from 11-1 start to 11-1 end

--- pinia-basic/src/stores/counter.ts
+++ pinia-basic/src/stores/counter.ts
@@ -1,12 +1,23 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+interface State {
+  counter: number
+}
 
-  return { count, doubleCount, increment }
+export const useCounterStore = defineStore({
+  id: 'counter',
+  state: (): State => {
+    return {
+      counter: 0
+    }
+  },
+  getters: {
+    doubleCount: (state) => state.counter * 2
+  },
+  actions: {
+    increment() {
+      this.counter++
+    }
+  }
 })
